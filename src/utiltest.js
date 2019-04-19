1f850b88187bceb83800facef8d0cebe714ace72
vector <vector.wang@hirede.com>
2019-04-19 19:34:40 +0800

    test:

--- src/utiltest.js
+++ src/utiltest.js
@@ -1,3 +1,7 @@
 const log = require('log-utils')
 
-console.log(log.error)
+console.log(log.error);
+console.log(log.info);
+console.log(log.success);
+console.log(log.warning);
+console.log(log.timestamp);
